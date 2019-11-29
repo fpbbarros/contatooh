8dec4b88c8dff8387c705c9c02d49c4f231e4802
Fábio Pereira Barros <44626039+fpbbarros@users.noreply.github.com>
2019-11-29 13:23:44 -0300

    cricao da views de error, contatos e contato

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,25 @@
+'use strict';
 
-angular.module('contatooh', []);
+angular.module('contatooh', ['ngRoute'])
+  .config(function ($routeProvider) {
+
+
+    $routeProvider.when('/contatos', {
+      templateUrl: './partials/contatos.html',
+      controller: 'ContatosController'
+    });
+
+
+    $routeProvider.when('/contato/:contatoId', {
+      templateUrl: './partials/contato.html',
+      controller: 'ContatoController'
+    });
+
+
+    $routeProvider.when('/error', {
+      templateUrl: './partials/error.html',
+
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/error' });
+  });
