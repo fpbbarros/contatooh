f215658f413ef400993aacd6289966d9656d2ca0
Fábio Pereira Barros <44626039+fpbbarros@users.noreply.github.com>
2019-11-29 16:37:10 -0300

    criando o filtro de busca

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,7 +1,39 @@
 
 angular.module('contatooh').controller('ContatosController',
   function ($scope) {
-    $scope.total = 0;
+
+
+    $scope.filtro = "";
+
+    $scope.contatos = [
+      {
+        "_id": 1,
+        "nome": "Contato Angular 1",
+        "email": "cont1@empresa.com.br"
+      },
+      {
+        "_id": 2,
+        "nome": "Contato Angular 2",
+        "email": "cont2@empresa.com.br"
+      },
+      {
+        "_id": 3,
+        "nome": "Contato Angular 3",
+        "email": "cont3@empresa.com.br"
+      },
+
+      {
+        "_id": 4,
+        "nome": "Contato Angular 4",
+        "email": "cont4@empresa.com.br"
+      },
+
+    ];
+
+
+
+
+    $scope.total = $scope.contatos.length;
     $scope.meuNome = "Fábio Pereira Barros";
 
     $scope.decrementa = () => {
